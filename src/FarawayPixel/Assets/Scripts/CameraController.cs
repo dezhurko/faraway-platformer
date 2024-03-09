b67a9284359aea20482facde784568583dacfe9b
Alexander Dezhurko <alex.d@quell.tech>
2024-03-09 12:27:19 +0300

    Implement draft player and camera controllers.

---
+++ src/FarawayPixel/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace Faraway.Pixel
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform targetTransform;
+        [SerializeField]
+        private float lerp = 0.7f;
+
+        private float initialCameraZPos;
+
+        private void Start()
+        {
+            initialCameraZPos = transform.position.z;
+        }
+
+        private void Update()
+        {
+            var target = targetTransform.position;
+            transform.position = Vector3.Lerp(
+                transform.position, 
+                new Vector3(target.x, target.y, initialCameraZPos), 
+                lerp * Time.deltaTime * 60f);
+        }
+    }
+}
